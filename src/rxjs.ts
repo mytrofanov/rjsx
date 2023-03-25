10b95824c310deb4401b3e5d6ab949a72f841eb2
mytrofanov <mytrofanovmaxym@gmail.com>
2023-03-25 20:16:53 +0200

    feat: points

--- src/rxjs.ts
+++ src/rxjs.ts
@@ -1,3 +1,5 @@
+import {fromEvent, map, pairwise, switchMap} from 'rxjs'
+
 export function Rxjs(selector: string) {
 
     const canvas = document.getElementById(selector) as HTMLCanvasElement;
@@ -16,6 +18,30 @@ export function Rxjs(selector: string) {
     }
 
     function drawLine([prev, next]: Position[]) {
-
+        if (!cx) return;
+        //start drawing
+        cx.beginPath();
+        cx.moveTo(prev.x, prev.y); //start line from
+        cx.lineTo(next.x, next.y)  //end of line
+        cx.stroke();
     }
+
+    const mouseMove$ = fromEvent<MouseEvent>(canvas, 'mousemove');
+    const mouseDown$ = fromEvent<MouseEvent>(canvas, 'mousedown');
+    const mouseUp$ = fromEvent<MouseEvent>(canvas, 'mouseup');
+    const mouseOut$ = fromEvent<MouseEvent>(canvas, 'mouseout');
+
+    const points$ = mouseMove$.pipe(
+        map<MouseEvent, Position>(({clientX, clientY}) => {
+            return {
+                x: clientX,
+                y: clientY,
+            }
+        }),
+        pairwise<Position>()
+    )
+
+    mouseDown$.pipe(
+        switchMap(()=> points$)
+    ).subscribe(drawLine)
 }
